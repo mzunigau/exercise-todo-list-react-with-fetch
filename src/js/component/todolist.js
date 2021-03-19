68ccc6dc05196873fe9006b88ee26bf1a62a3d91
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-03-19 04:25:19 +0000

    lista

--- src/js/component/todolist.js
+++ src/js/component/todolist.js
@@ -3,36 +3,50 @@ import React, { useState, useEffect } from "react";
 export function TodoList() {
 	//para tareas individuales
 	const [task, setTask] = useState();
-
 	//para flag
 	const [flag, setFlag] = useState();
+	//usuario
+	const [user, setUser] = useState();
+	// URL EndPoint
+	const [url, setURL] = useState("");
 
 	//almacenar grupos de tareas
 	const [todoList, setTodoList] = useState([]);
 
-	const handleChange = () => {
-		if (todoList != "null") {
-			setTodoList(newTask => [...newTask, { label: task, done: false }]);
-			setTask("");
-			update(todoList);
-		}
+	const handleChange = e => {
+		setTask(e.target.value);
+		setTodoList(newTask => [...newTask, { label: task, done: false }]);
+		console.log(todoList);
+		setTask("");
+		update();
+		//getData();
+	};
+
+	const handleUser = e => {
+		setUser(e.target.value);
+		setURL(`https://assets.breatheco.de/apis/fake/todos/user/${user}`);
+		create();
+		//getData();
 	};
 
 	const deleteTask = id => {
 		todoList.splice(id, 1);
-		setTodoList([...todoList]);
+		setTodoList([todoList]);
+		update();
+		if (flag) {
+			getData();
+		}
 	};
 
 	//Init
 	useEffect(() => {
-		create();
 		getData();
 	}, []);
 
 	//Services
 	//show
 	const getData = () => {
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/mzuniga103", {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/marco106", {
 			method: "GET",
 			headers: {
 				"Content-Type": "application/json",
@@ -43,12 +57,12 @@ export function TodoList() {
 				return response.json();
 			})
 			.then(function(myJson) {
-				setTodoList(todoList.concat(myJson));
+				setTodoList(myJson);
 			});
 	};
 	//Create User
 	const create = () => {
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/mzuniga103", {
+		fetch(url, {
 			method: "POST",
 			body: JSON.stringify([]),
 			headers: {
@@ -58,7 +72,7 @@ export function TodoList() {
 	};
 	// Update users tasks
 	const update = () => {
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/mzuniga103", {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/marco106", {
 			method: "PUT",
 			body: JSON.stringify(todoList),
 			headers: {
@@ -66,23 +80,20 @@ export function TodoList() {
 			}
 		})
 			.then(resp => {
-				console.log(resp.ok); // will be true if the response is successfull
-				console.log(resp.status); // the status code = 200 or code = 400 etc.
-				console.log(resp.text()); // will try return the exact result as string
-				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+				return resp.json();
 			})
 			.then(data => {
-				setTodoList(data);
+				console.log(data);
+				setFlag(true);
 			})
 			.catch(error => {
-				//error handling
 				console.log(error);
 			});
 	};
 
 	//Delete
 	const deleteT = () => {
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/mzuniga103", {
+		fetch(url, {
 			method: "DELETE",
 			headers: {
 				"Content-Type": "application/json"
@@ -93,16 +104,40 @@ export function TodoList() {
 	return (
 		<div className="col d-flex justify-content-center todo">
 			<div className="card  text-center" style={{ width: "18rem" }}>
-				<div className="card-header"> TodoList </div>
+				<div className="card-header">
+					{" "}
+					<h1>TodoList</h1>{" "}
+					<input
+						className="form-control "
+						placeholder="Seleccionar usuario"
+						onKeyPress={e =>
+							e.key === "Enter" ? handleUser(e) : ""
+						}
+						onChange={e => setUser(e.target.value)}
+						value={user}
+					/>
+					<button
+						type="button"
+						onClick={() => deleteT()}
+						className="btn btn-danger mt-3">
+						Eliminar Listado
+					</button>{" "}
+					<button
+						type="button"
+						onClick={() => getData()}
+						className="btn btn-success mt-3">
+						Refresh
+					</button>{" "}
+				</div>
 
 				<ul className="list-group list-group-flush">
 					<li className="list-group-item">
 						{" "}
 						<input
-							className="taskbox"
+							className="taskbox form-control"
 							placeholder="Escribe tus tareas"
 							onKeyPress={e =>
-								e.key === "Enter" ? handleChange() : ""
+								e.key === "Enter" ? handleChange(e) : ""
 							}
 							onChange={e => setTask(e.target.value)}
 							value={task}
@@ -110,11 +145,22 @@ export function TodoList() {
 					</li>
 
 					{todoList.map((result, i) => (
-						<li className="list-group-item" key={i}>
-							{result.label}{" "}
-							<span>
-								{result.done ? "Terminada" : "Sin terminar"}
-							</span>
+						<li className="list-group-item task" key={i}>
+							<div className="container">
+								<div className="row">
+									<div
+										className="col"
+										onClick={() => deleteTask(result.id)}>
+										&#10003;
+										{result.label}
+									</div>
+									<div
+										className="close col col-lg-1"
+										onClick={() => deleteTask(result.id)}>
+										&times;
+									</div>
+								</div>
+							</div>
 						</li>
 					))}
 
